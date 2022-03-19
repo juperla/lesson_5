442ea30b120481192bfd281b90f3a16bb5b75e07
Юлия <juperla@bk.ru>
2022-03-20 00:25:28 +0700

    20/03/2022

--- src/test/java/DemoqaTests.java
+++ src/test/java/DemoqaTests.java
@@ -3,9 +3,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -46,8 +44,7 @@ public class DemoqaTests {
         String lastName = "Super";
         String userEmail = "email@yahoo.com";
         String userNumber = "9139111111";
-        String subjectsInput = "C";
-        String currentAddress = "USA";
+
 
         driver.findElement(By.xpath("//*[@id='firstName']")).click();
         driver.findElement(By.id("firstName")).sendKeys(firstName);
@@ -74,30 +71,34 @@ public class DemoqaTests {
 
         driver.findElement(By.xpath("//*[@id='dateOfBirth']/div[2]/div[2]/div/div/div[2]/div[2]/div[3]/div[5]")).click();
 
+        WebElement subjectsAutocomplete = driver.findElement(By.id("subjectsInput"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", subjectsAutocomplete);
+        subjectsAutocomplete.sendKeys("Chemistry");
+        subjectsAutocomplete.sendKeys(Keys.ENTER);
 
-        driver.findElement(By.xpath("//*[@id='subjectsInput']")).click();
-        driver.findElement(By.id("subjectsInput")).sendKeys(subjectsInput);
-        WebElement selectElement = driver.findElement(By.id("subjectsInput"));
-        Select select2 = new Select(selectElement);
-        select2.selectByVisibleText("Chemistry");
-
-        WebElement checkBox = driver.findElement(By.xpath("//*[@id='hobbiesWrapper']/div[2]/div[2]/label"));
-        checkBox.click();
+        driver.findElement(By.xpath("//*[@id='hobbiesWrapper']/div[2]/div[2]/label")).click();
 
         File file = new File("src/image.png.jpg");
         WebElement selectPictureButton = driver.findElement(By.id("uploadPicture"));
         selectPictureButton.sendKeys(file.getAbsolutePath());
 
-        // driver.findElement(By.xpath("//*[@id='currentAddress']")).click();
-        // driver.findElement(By.id("currentAddress")).sendKeys(currentAddress);
+        WebElement subjectsAutocomplete1 = driver.findElement(By.id("currentAddress"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", subjectsAutocomplete);
+        subjectsAutocomplete1.sendKeys("Russia, Novosibirsk");
+        subjectsAutocomplete1.sendKeys(Keys.ESCAPE);
 
-        WebElement selectElement2 = driver.findElement(By.xpath("//*[@id='state']"));
-        Select select3 = new Select(selectElement2);
-        select3.selectByVisibleText("Haryana");
+        // driver.findElement(By.id("currentAddress")).sendKeys("Russia");
+
+        WebElement selectElement3 = driver.findElement(By.xpath("//*[@id='state']"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", selectElement3);
+        Select select3 = new Select(selectElement3);
+        select3.selectByIndex(3);
 
         WebElement selectElement4 = driver.findElement(By.id("city"));
         Select select4 = new Select(selectElement4);
         select4.selectByVisibleText("Karnal");
+
+        driver.findElement(By.id("submit")).click();
 
 
         //  Assertions.assertEquals("Haryana", allOption.get(3).getText());
